d324a377ca1c86e7934e97b744753f5656b6d917
Hankyul Min <denirokr22@gmail.com>
2023-06-19 11:45:51 +0900

    Feat: 랜딩페이지 링크 페이지 Card 컴포넌트

---
+++ src/pages/About/Link/components/Card.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="root">
+      <div className="card">
+        <div className="container">
+          <span className="title">SITE</span>
+          <span className="content" alt="지진알리미-FE">
+            <a className="item">지진알리미-FE</a>
+            <a
+              className="item"
+              href="https://earthquake-alert.site/"
+              alt="지진알리미-FE&BE"
+            >
+              지진알리미-FE&BE
+            </a>
+          </span>
+        </div>
+        <div className="container">
+          <span className="title">GITHUB</span>
+          <span className="content">
+            <a
+              className="item"
+              href="https://github.com/dgu-earthquake-alert/2023-1R-team-project.git"
+              alt="지진알리미-FE, 랜딩페이지 깃허브"
+            >
+              지진알리미-FE, 랜딩페이지
+            </a>
+            <a
+              className="item"
+              href="https://github.com/dgu-earthquake-alert/earthquake-alert.git"
+              alt="지진알리미-FE&BE"
+            >
+              지진알리미-FE&BE
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
