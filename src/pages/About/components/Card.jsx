56b7329b8b69444db0a3127665715616aeb96efe
Kong-E <kng001016@dgu.ac.kr>
2023-06-19 15:49:40 +0900

    Chore: 카드 컴포넌트 이미지 사이즈 고정 및 디렉토리 구조 수정

--- src/pages/About/components/Card.jsx
+++ src/pages/About/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../../styles/about/card.css";
+import "./styles/card.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Card = ({ data }) => {
@@ -8,19 +8,28 @@ const Card = ({ data }) => {
   return (
     <div>
       <div className="card mb-3 card_box">
-        <div className="row g-0">
+        <div className="row g-0" style={{ width: "100%" }}>
           <div className="col-md-4 img_wrapper">
-            <img src={data.img} className="img-fluid img rounded-circle" />
+            <img
+              src={data.img}
+              className="img-fluid rounded-circle"
+              alt="profile"
+              style={{ width: "350px", height: "350px", objectFit: "cover" }}
+            />
           </div>
           <div className="col-md-8">
             <div className="card-body">
               <h3 className="card-title">{data.name}</h3>
               <p className="card-text">Student ID: {data.studentId}</p>
               <p className="card-text">Department: {data.dept}</p>
-              <p className="card-text">Skills: </p>
-              {data.skills.map((skill, index) => (
-                <img key={index} src={skill} alt="Skill" />
-              ))}
+              <p className="card-text">
+                Skills:
+                <div>
+                  {data.skills.map((skill, index) => (
+                    <img key={index} src={skill} alt="Skill" />
+                  ))}
+                </div>
+              </p>
               <p className="card-text">
                 Role: <br />
                 {roleLines.map((line, index) => (
